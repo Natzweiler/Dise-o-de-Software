9e5e7a22b1a30efce8666e2a52749bc16590b2ce
Natzweiler <ethangael06@gmail.com>
2025-04-01 18:54:26 -0700

    proyecto DTO Negocio
    Se creó el proyecto DTONegocio

---
+++ objetosNegocio_Equipo1/src/objetosnegocio_equipo1/ObjetosNegocio_Equipo1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package objetosnegocio_equipo1;
+
+/**
+ *
+ * @author Gael
+ */
+public class ObjetosNegocio_Equipo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
